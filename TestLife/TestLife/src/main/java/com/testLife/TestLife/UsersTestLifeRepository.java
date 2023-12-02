f844e2cb99671ef83b15d8b334f952ec8d3d5528
Naman Gupta <gnaman38@gmail.com>
2023-12-02 19:10:15 +0530

    Delete TestLife/TestLife directory

--- TestLife/TestLife/src/main/java/com/testLife/TestLife/UsersTestLifeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.testLife.TestLife;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsersTestLifeRepository extends CrudRepository<users,Integer>{
-	users findByEmail(String email);
-}
